48fd110e45263b3b5d824545c5067a95fa755529
Lucas <LucasClavel356@gmail.com>
2022-10-19 12:51:42 +0200

    fixed small mistake

--- Kaart/Coordinaat.cs
+++ Kaart/Coordinaat.cs
@@ -3,7 +3,15 @@ public struct Coordinaat{
     public readonly int x;
     public readonly int y;
     public Coordinaat(int xx, int yy){
-        x = xx; 
-        y = yy;
+        if (xx < 0){
+            x = 0;
+        }else{
+            x = xx;
+        }
+        if (yy < 0){
+            y = 0;
+        }else{
+            y = yy;
+        }
     }
 }
